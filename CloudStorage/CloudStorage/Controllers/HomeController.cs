44cdd7667cda611f95533bae88d0132ae9c0dbee
Bogdan Rotaru <bogdan.rotaru13@yahoo.com>
2018-10-23 20:48:33 +0300

    First commit, same front-end
    first commit (after adding .gitignore to fix error)

---
+++ CloudStorage/CloudStorage/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CloudStorage.Models;
+
+namespace CloudStorage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Insert()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Mydocs()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            ViewData["Message"] = "Here will be login form.";
+
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            ViewData["Message"] = "Here you need to register.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
